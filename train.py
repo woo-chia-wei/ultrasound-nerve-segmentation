697d4550be720a1ff261f8cbe5e8b890041c5168
woo-chia-wei <chiaweiwoo123@gmail.com>
2018-08-12 14:25:21 +0800

    Updated the obsolete syntax

--- train.py
+++ train.py
@@ -1,23 +1,23 @@
-from __future__ import print_function
+    from __future__ import print_function
 
-import cv2
-import numpy as np
-from keras.models import Model
-from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D
-from keras.layers.core import Dropout
-from keras.optimizers import *
-from keras.constraints import maxnorm
-from keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from keras import backend as K
-from keras.utils.visualize_util import plot
-from ImageDataGenerator import ImageDataGenerator
+    import cv2
+    import numpy as np
+    from keras.models import Model
+    from keras.layers import Input, merge, Convolution2D, MaxPooling2D, UpSampling2D, concatenate
+    from keras.layers.core import Dropout
+    from keras.optimizers import *
+    from keras.constraints import maxnorm
+    from keras.callbacks import ModelCheckpoint, LearningRateScheduler
+    from keras import backend as K
+    from keras.utils.vis_utils import plot_model
+    from ImageDataGenerator import ImageDataGenerator
 
-from data import load_train_data, load_test_data
+    from data import load_train_data, load_test_data
 
-img_rows = 64
-img_cols = 96
+    img_rows = 64
+    img_cols = 96
 
-smooth = 1. 
+    smooth = 1. 
 
 
 def dice_coef(y_true, y_pred):
@@ -30,61 +30,60 @@ def dice_coef(y_true, y_pred):
 def dice_coef_loss(y_true, y_pred):
     return 1. / (dice_coef(y_true, y_pred) + smooth)
 
-
 def get_unet():
-    inputs = Input((1, img_rows, img_cols))
-    conv1 = Convolution2D(64, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(inputs)
-    conv1 = Convolution2D(64, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv1)
+    inputs = Input((img_rows, img_cols, 1))
+    conv1 = Convolution2D(64, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(inputs)
+    conv1 = Convolution2D(64, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv2 = Convolution2D(128, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(pool1)
-    conv2 = Convolution2D(128, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv2)
+    conv2 = Convolution2D(128, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(pool1)
+    conv2 = Convolution2D(128, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv2)
     pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
-    conv3 = Convolution2D(256, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(pool2)
-    conv3 = Convolution2D(256, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv3)
+    conv3 = Convolution2D(256, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(pool2)
+    conv3 = Convolution2D(256, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv3)
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
 
-    conv4 = Convolution2D(512, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(pool3)
-    conv4 = Convolution2D(512, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv4)
+    conv4 = Convolution2D(512, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(pool3)
+    conv4 = Convolution2D(512, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
-    conv5 = Convolution2D(1024, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(pool4)
+    conv5 = Convolution2D(1024, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(pool4)
     conv5 = Dropout(0.5)(conv5)
-    conv5 = Convolution2D(1024, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv5)
+    conv5 = Convolution2D(1024, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv5)
     conv5 = Dropout(0.5)(conv5)
     pool5 = MaxPooling2D(pool_size=(2, 2))(conv5)
 
-    conv05 = Convolution2D(2048, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(pool5)
+    conv05 = Convolution2D(2048, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(pool5)
     conv05 = Dropout(0.5)(conv05)
-    conv05 = Convolution2D(2048, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv05)
+    conv05 = Convolution2D(2048, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv05)
     conv05 = Dropout(0.5)(conv05)
 
-    up6 = merge([UpSampling2D(size=(2, 2))(conv05), conv5], mode='concat', concat_axis=1)
-    conv6 = Convolution2D(1024, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(up6)
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv05), conv5])
+    conv6 = Convolution2D(1024, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(up6)
     conv6 = Dropout(0.5)(conv6)
-    conv6 = Convolution2D(1024, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv6)
+    conv6 = Convolution2D(1024, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv6)
     conv6 = Dropout(0.5)(conv6)
 
-    up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv4], mode='concat', concat_axis=1)
-    conv7 = Convolution2D(512, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(up7)
-    conv7 = Convolution2D(512, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv7)
+    up7 = concatenate([UpSampling2D(size=(2, 2))(conv6), conv4])
+    conv7 = Convolution2D(512, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(up7)
+    conv7 = Convolution2D(512, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv7)
 
-    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv3], mode='concat', concat_axis=1)
-    conv8 = Convolution2D(256, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(up8)
-    conv8 = Convolution2D(256, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv8)
+    up8 = concatenate([UpSampling2D(size=(2, 2))(conv7), conv3])
+    conv8 = Convolution2D(256, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(up8)
+    conv8 = Convolution2D(256, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv8)
 
-    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv2], mode='concat', concat_axis=1)
-    conv9 = Convolution2D(128, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(up9)
-    conv9 = Convolution2D(128, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv9)
+    up9 = concatenate([UpSampling2D(size=(2, 2))(conv8), conv2])
+    conv9 = Convolution2D(128, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(up9)
+    conv9 = Convolution2D(128, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv9)
 
-    up10 = merge([UpSampling2D(size=(2, 2))(conv9), conv1], mode='concat', concat_axis=1)
-    conv09 = Convolution2D(64, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(up10)
-    conv09 = Convolution2D(64, 3, 3, activation='relu', init='lecun_uniform', W_constraint=maxnorm(3), border_mode='same')(conv09)
+    up10 = concatenate([UpSampling2D(size=(2, 2))(conv9), conv1])
+    conv09 = Convolution2D(64, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(up10)
+    conv09 = Convolution2D(64, (3, 3), activation='relu', kernel_initializer='lecun_uniform', kernel_constraint=maxnorm(3), padding='same')(conv09)
 
-    conv10 = Convolution2D(1, 1, 1, activation='sigmoid')(conv09)
+    conv10 = Convolution2D(1, (1, 1), activation='sigmoid')(conv09)
 
-    model = Model(input=inputs, output=conv10)
+    model = Model(inputs=inputs, outputs=conv10)
 
     model.compile(optimizer=Adadelta(lr=0.1, rho=0.95, epsilon=1e-08), loss=dice_coef_loss, metrics=[dice_coef])
 
@@ -96,6 +95,10 @@ def preprocess(imgs):
     for i in range(imgs.shape[0]):
         imgs_p[i, 0]  = cv2.resize(imgs[i, 0], (img_cols, img_rows), interpolation=cv2.INTER_AREA)
     return imgs_p
+
+
+def swap_axes(imgs):
+    return np.swapaxes(np.swapaxes(imgs, 1, 2), 2, 3)
 
 
 def train_and_predict():
@@ -121,7 +124,6 @@ def train_and_predict():
     print('Creating and compiling model...')
     print('-'*30)
     model = get_unet()
-    plot(model, to_file='model.png', show_shapes=True)
     model_checkpoint = ModelCheckpoint('unet.hdf5', monitor='loss', save_best_only=True)
 
     print('-'*30)
@@ -132,8 +134,8 @@ def train_and_predict():
             vertical_flip=True,
             horizontal_flip=True,
             )
-    model.fit_generator(datagen.flow(imgs_train, imgs_mask_train, batch_size=32, shuffle=True),
-            samples_per_epoch=len(imgs_train), nb_epoch=120, verbose=1, callbacks=[model_checkpoint])
+    model.fit_generator(datagen.flow(swap_axes(imgs_train), imgs_mask_train, batch_size=32, shuffle=True),
+        steps_per_epoch=len(imgs_train), epochs=2, verbose=1, callbacks=[model_checkpoint])
 
 
     print('-'*30)
@@ -154,7 +156,7 @@ def train_and_predict():
     print('-'*30)
     print('Predicting masks on test data...')
     print('-'*30)
-    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    imgs_mask_test = model.predict(swap_axes(imgs_test[:10]),  verbose=1)
     np.save('imgs_mask_test.npy', imgs_mask_test)
 
 
